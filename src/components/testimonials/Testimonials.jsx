bbae8a5467c9ce5c2b06660f4a3052b499f7e06f
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-03-30 17:24:40 +0530

    Basic outline for testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import css from "./Testimonials.module.css";
+import Hero from "../../assets/testimonialHero.png";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className={css.testimonials}>
+      <div className={css.wrapper}>
+        <span>Top Rated</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+      </div>
+      <img src="Hero" alt="" />
+
+      <div className={css.container}>
+        <span>100k</span>
+        <span>Happy Customers with us</span>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
