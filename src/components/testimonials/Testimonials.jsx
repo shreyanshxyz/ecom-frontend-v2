99252c0f8160c56e760929e2137a5a13c68b2f8f
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-01 15:41:04 +0530

    Testimonials Done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import css from "./Testimonials.module.css";
 import Hero from "../../assets/testimonialHero.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { TestimonialsData } from "../../data/testimonials";
 
 const Testimonials = () => {
   return (
     <div className={css.testimonials}>
       <div className={css.wrapper}>
-        <span>Top Rated</span>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        <div className={css.container}>
+          <span>Top Rated</span>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        </div>
+        <img src={Hero} alt="" />
+        <div className={css.container}>
+          <span>100k</span>
+          <span>Happy Customers with us</span>
+        </div>
       </div>
-      <img src="Hero" alt="" />
 
-      <div className={css.container}>
-        <span>100k</span>
-        <span>Happy Customers with us</span>
+      <div className={css.reviews}>Reviews</div>
+      <div className={css.carousel}>
+        <Swiper
+          slidesPerGroup={1}
+          slidesPerView={3}
+          spaceBetween={20}
+          className={css.tCarousel}
+        >
+          {TestimonialsData.map((testimonial, i) => (
+            <SwiperSlide>
+              <div className={css.testimonial}>
+                <img src={testimonial.image} alt="" />
+                <span>{testimonial.comment}</span>
+                <hr />
+                <span>{testimonial.name}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
